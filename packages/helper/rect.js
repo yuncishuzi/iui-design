bc836467d5538ad52475d3f0e8434a5951d57c44
Nobu <121139267@qq.com>
2023-08-08 09:50:55 +0800

    add license

--- packages/helper/rect.js
+++ packages/helper/rect.js
@@ -1,7 +1,5 @@
-// 获取元素节点信息
 export function getRect(context, selector, needAll = false) {
   return new Promise((resolve, reject) => {
-    // PC预览时，会将组件的高度设置为0，所以需要延迟获取
     setTimeout(() => {
       uni
         .createSelectorQuery()
