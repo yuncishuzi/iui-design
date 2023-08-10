158c644303f616a9c20f6186ca0410c3330d5e0c
nobu121 <hujianlin@iyunci.cn>
2023-08-10 19:02:55 +0800

    🎈 perf:The deferred get node information method is executed only under H5

--- packages/helper/rect.js
+++ packages/helper/rect.js
@@ -1,18 +1,22 @@
 export function getRect(context, selector, needAll = false) {
   return new Promise((resolve, reject) => {
+    // #ifdef H5
     setTimeout(() => {
-      uni
-        .createSelectorQuery()
-        .in(context)
-        [needAll ? "selectAll" : "select"](selector)
-        .boundingClientRect((rect) => {
-          if (rect) {
-            resolve(rect);
-          } else {
-            reject(rect);
-          }
-        })
-        .exec();
+    // #endif
+    uni
+      .createSelectorQuery()
+      .in(context)
+      [needAll ? "selectAll" : "select"](selector)
+      .boundingClientRect((rect) => {
+        if (rect) {
+          resolve(rect);
+        } else {
+          reject(rect);
+        }
+      })
+      .exec();
+    // #ifdef H5
     }, 5);
+    // #endif
   });
 }
